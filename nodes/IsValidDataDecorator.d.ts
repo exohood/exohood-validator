a630c5e42f1379bb0aecdb76122e3a51d926d255
JJ Uzumaki <104718343+xpunk7@users.noreply.github.com>
2022-07-07 14:24:20 +0200

    Create IsValidDataDecorator.d.ts

---
+++ nodes/IsValidDataDecorator.d.ts
@@ -0,0 +1,2 @@
+export declare function isValidData(target: any, propertyKey: string, parameterIndex: number): void;
+export declare function validate(target: any, propertyKey: string, descriptor: PropertyDescriptor): void;
